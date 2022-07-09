09445353299117c31ace6817d0dad12968aa4a7a
nazap31 <you@example.com>
2022-07-08 18:40:18 -0600

    arrays

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Array!");
+
+string [] weekDays = new string[7];
+Console.WriteLine(weekDays);
+Console.WriteLine(weekDays[0]);
+
+weekDays[0] = "Monday";
+weekDays[1] = "Tuesday";
+weekDays[2] = "Wednestay";
+weekDays[3] = "Thursday";
+weekDays[4] = "Friday";
+weekDays[5] = "Saturday";
+weekDays[6] = "Sunday";
+Console.WriteLine(weekDays[0]);
+Console.WriteLine(weekDays[3]);
+Console.WriteLine(weekDays[6]);
+
+int[] primes = new int [] {2,3,5,7,11,13};
+
+Console.WriteLine(primes[1]);
+
+int[] pairs = {2,4,6,8,10};
+Console.WriteLine(pairs[4]);
